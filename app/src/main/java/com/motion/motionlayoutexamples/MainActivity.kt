eb9d9449e2a4d9c5282da5934271c20ba3883e74
Parth Mehta <parth@techizer.in>
2020-12-10 17:07:02 +0530

    + Simple Recyclerview with header animation completed

--- app/src/main/java/com/motion/motionlayoutexamples/MainActivity.kt
+++ app/src/main/java/com/motion/motionlayoutexamples/MainActivity.kt
@@ -1,11 +1,33 @@
 package com.motion.motionlayoutexamples
 
+import android.content.Intent
 import androidx.appcompat.app.AppCompatActivity
 import android.os.Bundle
+import android.view.View
+import android.widget.Button
+import kotlinx.android.synthetic.main.activity_main.*
 
-class MainActivity : AppCompatActivity() {
+class MainActivity : AppCompatActivity(), View.OnClickListener {
+
+    /*To avoid this add kotlin extensions in app's build.gradle file*/
+    //var button1: Button?=null
+
     override fun onCreate(savedInstanceState: Bundle?) {
         super.onCreate(savedInstanceState)
         setContentView(R.layout.activity_main)
+
+       // button1=findViewById(R.id.button1)
+        button1!!.setOnClickListener(this@MainActivity)
+
+    }
+
+    override fun onClick(v: View?) {
+        when(v!!.id)
+        {
+            R.id.button1->{
+                var intent:Intent =Intent(this@MainActivity,Scrollheader::class.java)
+                startActivity(intent)
+            }
+        }
     }
 }
